dbcf1073f70497c6e21d963efa5241bb7227be06
Ethan Freeman <ethanfree23@gmail.com>
2022-03-12 14:09:06 -0600

    Compelted assingment

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,31 @@
+const { renderMatches } = require("react-router-dom");
+
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+        case "counter/increment":
+            return { count: state.count + 1 };
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    renderMatches();
+}
+
+function render() {
+    const container = document.getElementById("container");
+    container.textContent = state.count;
+}
+
+dispatch({ type: "@@INIT" });
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+    dispatch({ type: "counter/increment" })
+})
